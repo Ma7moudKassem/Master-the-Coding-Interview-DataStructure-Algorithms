f80146190ed9c5cb837a910bf8bf3617f257785e
Mahmoud Kassem <modykassem123@gmail.com>
2022-08-08 13:58:06 +0200

    Add project files.

---
+++ DataStructures_And_Algorithms/BigO/WhatIsGoodCode/FindMody.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructures_And_Algorithms.BigO.WhatIsGoodCode
+{
+    public class FindMody
+    {
+		public string findMody(string[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (arr[i] == "mody")
+				{
+					return "Mody has finded!!";
+				}
+			}
+			return "Ooh Sorry Mody hasn't findrd";
+		}
+	}
+}
---
+++ DataStructures_And_Algorithms/Program.cs
@@ -0,0 +1,7 @@
+﻿using DataStructures_And_Algorithms.BigO.WhatIsGoodCode;
+
+string[] mody = { "n" };
+FindMody obj = new FindMody();
+
+Console.WriteLine(obj.findMody(mody));
+//
